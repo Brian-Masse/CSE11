72c1d004c105e2291cb5fabf5a34513ffdc35066
Brian-Masse <50341047+Brian-Masse@users.noreply.github.com>
2024-04-06 16:01:21 -0700

    added PA1

---
+++ PA1/Assignment1.java
@@ -0,0 +1,15 @@
+package PA1;
+public class Assignment1 {
+    public static void main( String[] args ) {
+
+        String name = "Brian Masse";
+        String interest = "Drawing 🔖";
+
+        System.out.println( "hello world!" );
+
+        System.out.println( "Hi, my name is " + name );
+        System.out.println( "I like " + interest );
+        
+
+    }
+}
