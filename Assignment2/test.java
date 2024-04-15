33ca8ec49c95a344d63e67fea793ceb7bde22e11
Brian Masse <brianm25it@gmail.com>
2024-04-14 20:37:53 -0700

    added testing file

---
+++ Assignment2/test.java
@@ -0,0 +1,21 @@
+public class test<E> {
+ 
+    class Node<E> {
+
+        E value;
+
+        public Node( E value ) {
+            this.value = value;
+        }
+
+    }
+
+    public static void main (String[] args) {
+
+
+
+    }
+
+
+
+}
